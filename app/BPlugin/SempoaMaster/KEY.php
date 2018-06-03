28a88f8b466d687812decb6dbd300c6f35087d29
pipoo <efindi.ongso@gmail.com>
2018-06-03 22:43:03 +0700

    memperpanjang undo spp

--- app/BPlugin/SempoaMaster/KEY.php
+++ app/BPlugin/SempoaMaster/KEY.php
@@ -73,8 +73,8 @@ class KEY
 // UNDO First Payment
 
     public static $MAX_UNDO_FIRST_PAYMENT = 30;
-    public static $MAX_UNDO_SPP = 90;
-
+//    public static $MAX_UNDO_SPP = 90;
+    public static $MAX_UNDO_SPP = 180;
     public static $MAX_UNDO_IURAN_BUKU = 90;
     /*
      * Load
